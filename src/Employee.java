57809317ed1f4d256b94b9d85394e218e67e3ffa
Ivan <ivansalnikov90@gmail.com>
2023-12-10 11:32:13 +0300

    first commit

---
+++ src/Employee.java
@@ -0,0 +1,80 @@
+import java.util.Objects;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    // Конструкторы
+    public Employee(String firstName, String lastName, int age, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public Employee(String firstName, String lastName, int age) {
+        this(firstName, lastName, age, 1_000.0);
+    }
+
+    public Employee(String firstName, String lastName) {
+        this(firstName, lastName, 25, 1_000.0);
+    }
+
+    // Геттеры
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    // Сеттеры
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                '}';
+    }
+
+    // equals и hashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(firstName, employee.firstName) &&
+                Objects.equals(lastName, employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+}
